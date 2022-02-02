4421ed3155ddff409ff2cb368981bdcc189ebe74
Desmond Madden <desmond.madden@hotmail.co.uk>
2022-02-02 20:52:20 +0000

    Test tidy up by using global variables

--- core/tests/core_test.py
+++ core/tests/core_test.py
@@ -1,38 +1,27 @@
 from main.core import Core
 from main.reg import Reg
 
+reg1 = Reg()
+reg2 = Reg()
+core = Core()
+
 def test_add():
-    reg1 = Reg()
     reg1.update_value(100)
-
-    reg2 = Reg()
     reg2.update_value(200)
-
-    core = Core()
     core.add("R1", reg1, reg2)
     
     assert core.get_value("R1") == 100 + 200
 
 def test_sub():
-    reg1 = Reg()
     reg1.update_value(100)
-
-    reg2 = Reg()
     reg2.update_value(200)
-
-    core = Core()
     core.sub("R1", reg1, reg2)
     
     assert core.get_value("R1") == 100 - 200
 
 def test_rsb():
-    core = Core()
-    reg1 = Reg()
     reg1.update_value(20)
-
-    reg2 = Reg()
     reg2.update_value(230)
-
     core.rsb("R1", reg1, reg2)
 
     assert core.get_value("R1") == 230 - 20
--- core/tests/reg_test.py
+++ core/tests/reg_test.py
@@ -1,10 +1,5 @@
 from main.reg import Reg
 
-def test_setting_reg():
-    reg = Reg()
-    reg.update_value(50)
-    assert reg.get_value() == 50
-
 def test_updating_reg_value():
     reg = Reg()
     reg.update_value(50)
